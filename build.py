b2fb28396e60481a6937fc2b3c3cc05d92fee57b
Anthony <awmorp@gmail.com>
2016-04-10 19:10:13 +1000

    Disable building language generators to speed up build

--- build.py
+++ build.py
@@ -175,11 +175,12 @@ class Gen_compressed(threading.Thread):
   def run(self):
     self.gen_core()
     self.gen_blocks()
-    self.gen_generator("javascript")
-    self.gen_generator("python")
-    self.gen_generator("php")
-    self.gen_generator("dart")
-    self.gen_generator("lua")
+# AWM: disable languages to reduce build time
+#    self.gen_generator("javascript")
+#    self.gen_generator("python")
+#    self.gen_generator("php")
+#    self.gen_generator("dart")
+#    self.gen_generator("lua")
 
   def gen_core(self):
     target_filename = "blockly_compressed.js"
