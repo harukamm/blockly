5992f6d2d8fe7d39b5d9bc288c3ef8855cc15092
Stefan Jacholke <stefanjacholke@gmail.com>
2016-06-19 23:13:34 +0200

    text color

--- blocks/text.js
+++ blocks/text.js
@@ -32,7 +32,7 @@ goog.require('Blockly.Blocks');
 /**
  * Common HSV hue for all blocks in this category.
  */
-Blockly.Blocks.texts.HUE = 160;
+Blockly.Blocks.texts.HUE = 45;
 
 Blockly.Blocks['text'] = {
   /**
