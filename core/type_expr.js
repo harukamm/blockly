462428ad9f5c2ba535108cc460d6c49844b05ad6
Stefan Jacholke <stefanjacholke@gmail.com>
2016-07-26 23:25:21 +0200

    Saving and loading to dom for TypeExprs

--- core/type_expr.js
+++ core/type_expr.js
@@ -14,6 +14,29 @@ Blockly.TypeExpr = function(name, children /* = [] */ ) {
   this.inputs = [];
 }
 
+Blockly.TypeExpr.prototype.toDom = function(){
+   var typeDom = goog.dom.createDom('type');
+   typeDom.setAttribute('name', this.name);
+
+   this.children.forEach(function(child){
+      typeDom.appendChild(child.toDom());
+   });
+
+   return typeDom;
+}
+
+Blockly.TypeExpr.fromDom = function(element){
+  var name = element.getAttribute('name');
+  var childNodes = element.childNodes;
+
+  var children = []
+  childNodes.forEach(function(childNode){
+    children.push(Blockly.TypeExpr.fromDom(childNode));
+  });
+
+  return new Blockly.TypeExpr(name,children);
+}
+
 Blockly.TypeExpr.prototype.isTypeVar = function() {
   return this.name in Blockly.TypeVar.getTypeVarDB_();
 }
