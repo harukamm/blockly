3e1abf9c47403dfe490c098d3c828e909e3a69cb
Stefan Jacholke <stefanjacholke@gmail.com>
2016-08-05 22:47:14 +0200

    Move program blocks

--- blocks/typed-blocks.js
+++ blocks/typed-blocks.js
@@ -1563,37 +1563,6 @@ Blockly.Blocks['fieldVarInputTest'] = {
 };
 
 
-Blockly.Blocks['cwSimulationOf'] = {
-  /**
-   * Block for comparison operator.
-   * @this Blockly.Block
-   */
-  init: function() {
-    this.setColour(0);
-    this.setOutput(false);
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldLabel('Simulation Of', 'blocklyTextEmph'));
-    
-    var world = Blockly.TypeVar.getUnusedTypeVar();
-    var number = new Blockly.TypeExpr('Number');
-    var listNum = new Blockly.TypeExpr('list', [new Blockly.TypeExpr('Number')]);
-    this.appendValueInput('INITIAL')
-        .setTypeExpr(new Blockly.TypeExpr('Function_', 
-              [new Blockly.TypeExpr('list', [new Blockly.TypeExpr('Number') ] ), world ]  ));
-
-    this.appendValueInput('STEP')
-        .setTypeExpr(new Blockly.TypeExpr('Function_', 
-              [world, new Blockly.TypeExpr('Number'), world ]  ));
-    this.appendValueInput('DRAW')
-        .setTypeExpr(new Blockly.TypeExpr('Function_', 
-              [world, new Blockly.TypeExpr('Picture') ]  ));
-    this.setInputsInline(true);
-    this.functionName = "";
-  }
-};
-
-
-
 
 /**
  * Pairs
