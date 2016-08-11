41396eb3e0297be2831e3e8477bce656596938a7
Stefan Jacholke <stefanjacholke@gmail.com>
2016-08-11 22:05:18 +0200

    Add interaction of block

--- blocks/cw-program.js
+++ blocks/cw-program.js
@@ -49,6 +49,43 @@ Blockly.Blocks['cwSimulationOf'] = {
   }
 };
 
+Blockly.Blocks['cwInteractionOf'] = {
+  /**
+   * Block for comparison operator.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.setColour(0);
+    this.setOutput(false);
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldLabel('Interaction Of', 'blocklyTextEmph'));
+    
+    var world = Blockly.TypeVar.getUnusedTypeVar();
+    var number = new Blockly.TypeExpr('Number');
+    var listNum = new Blockly.TypeExpr('list', [new Blockly.TypeExpr('Number')]);
+    this.appendValueInput('INITIAL')
+        .setTypeExpr(new Blockly.TypeExpr('Function_', 
+              [new Blockly.TypeExpr('list', [new Blockly.TypeExpr('Number') ] ), world ]  ));
+
+    this.appendValueInput('STEP')
+        .setTypeExpr(new Blockly.TypeExpr('Function_', 
+              [world, new Blockly.TypeExpr('Number'), world ]  ));
+
+    this.appendValueInput('EVENT')
+        .setTypeExpr(new Blockly.TypeExpr('Function_', 
+              [world, new Blockly.TypeExpr('Event'), world ]  ));
+
+    this.appendValueInput('DRAW')
+        .setTypeExpr(new Blockly.TypeExpr('Function_', 
+              [world, new Blockly.TypeExpr('Picture') ]  ));
+    this.setInputsInline(true);
+    this.functionName = "";
+  }
+};
+
+
+
+
 Blockly.Blocks['cwAnimationOf'] = {
   /**
    * Block for comparison operator.
